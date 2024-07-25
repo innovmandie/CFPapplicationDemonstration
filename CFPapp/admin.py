75ddd1851ed93d9db01dc1015788c317087f92b4
lostshadow <graph.rahal@live.fr>
2024-07-25 11:16:19 +0200

    start application
    add files to repository

---
+++ CFPapp/admin.py
@@ -0,0 +1,60 @@
+from django.contrib import admin
+from .models import PostA1_C1, PostA1_C2, PostA1_C3, PostA2_C1, PostA2_C2, PostA2_C3, PostA3_C1, PostA3_C2, PostA3_C3, PostA4_C1, PostA4_C2, PostA4_C3, PostA5_C1, PostA5_C2, PostA5_C3, PostA6_C1, PostA6_C2, PostA6_C3
+from .models import PostB1_C1, PostB1_C2,PostB1_C3,PostB2_C1, PostB2_C2, PostB2_C3, PostB3_C1, PostB3_C2, PostB3_C3, PostB4_C1, PostB4_C2, PostB4_C3
+from .models import PostC1_C1, PostC1_C2,PostC1_C3,PostC2_C1, PostC2_C2, PostC2_C3, PostC3_C1, PostC3_C2, PostC3_C3, PostC4_C1, PostC4_C2, PostC4_C3
+
+from .models import User_infos
+from .models import CFP_infos
+
+
+#admin.site.register(Utilisateur)
+admin.site.register(User_infos)
+admin.site.register(CFP_infos)
+
+
+admin.site.register(PostA1_C1)
+admin.site.register(PostA1_C2)
+admin.site.register(PostA1_C3)
+admin.site.register(PostA2_C1)
+admin.site.register(PostA2_C2)
+admin.site.register(PostA2_C3)
+admin.site.register(PostA3_C1)
+admin.site.register(PostA3_C2)
+admin.site.register(PostA3_C3)
+admin.site.register(PostA4_C1)
+admin.site.register(PostA4_C2)
+admin.site.register(PostA4_C3)
+admin.site.register(PostA5_C1)
+admin.site.register(PostA5_C2)
+admin.site.register(PostA5_C3)
+admin.site.register(PostA6_C1)
+admin.site.register(PostA6_C2)
+admin.site.register(PostA6_C3)
+
+
+
+admin.site.register(PostB1_C1)
+admin.site.register(PostB1_C2)
+admin.site.register(PostB1_C3)
+admin.site.register(PostB2_C1)
+admin.site.register(PostB2_C2)
+admin.site.register(PostB2_C3)
+admin.site.register(PostB3_C1)
+admin.site.register(PostB3_C2)
+admin.site.register(PostB3_C3)
+admin.site.register(PostB4_C1)
+admin.site.register(PostB4_C2)
+admin.site.register(PostB4_C3)
+
+admin.site.register(PostC1_C1)
+admin.site.register(PostC1_C2)
+admin.site.register(PostC1_C3)
+admin.site.register(PostC2_C1)
+admin.site.register(PostC2_C2)
+admin.site.register(PostC2_C3)
+admin.site.register(PostC3_C1)
+admin.site.register(PostC3_C2)
+admin.site.register(PostC3_C3)
+admin.site.register(PostC4_C1)
+admin.site.register(PostC4_C2)
+admin.site.register(PostC4_C3)
---
+++ CFPapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CfpappConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "CFPapp"
